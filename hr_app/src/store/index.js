cf7b3e59ff643efdb539a627382dad475b2a8ef3
rigalsisonke-prog <rigalasisonke@gmail.com>
2025-12-08 10:46:13 +0200

    created a store file and  imported vuex and linked the file to index.js where I imported each information given in the dummy data to make it easy for link data in each component,linked the store file to teh main.js as well

---
+++ hr_app/src/store/index.js
@@ -0,0 +1,21 @@
+import {createStore} from 'vuex'
+import employee_infoPage from './employee_info.json'
+import attendancePage from './attendance.json'
+import employee_payrollPage from './payroll_data.json'
+const store = createStore({
+    state:{
+        employees: employee_infoPage.employeeInformation,
+        employeesAttendance: attendancePage.attendanceAndLeave,
+        employeePayroll:employee_payrollPage.payrollData
+    },
+    getters:{
+
+    },
+    mutations:{
+
+    },
+    actions:{
+
+    }
+})
+export default store
