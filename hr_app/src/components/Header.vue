d4bb47ef2802d79740387d350cdca1036096b0e6
joshuajacobs717-maker <joshuajacobs717@gmail.com>
2025-12-08 11:27:37 +0200

    Data

---
+++ hr_app/src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+<div>
+    <div id="logo">
+        <h3></h3>
+    </div>
+    <div id="profile">
+        <img>
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
